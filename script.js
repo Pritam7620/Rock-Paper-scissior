e41ccb7ecf1f9784c64ece4b2443335652506614
Pritam7620 <chambharepritam98@gmail.com>
2026-02-28 00:12:39 +0530

    build Rock-paper-scissor game

---
+++ script.js
@@ -0,0 +1,52 @@
+let arr = ["Rock", "Paper", "Scissors"];
+
+function PlayGames(userchoice) {
+  console.log(userchoice);
+
+  let RandomChoice = Math.floor(Math.random() * 3);
+
+  let ComputerChoice = arr[RandomChoice];
+
+  let wins = "";
+  let draw = "";
+  let usercount = 0;
+  let computercount = 0;
+  if (
+    (userchoice == "Rock" && ComputerChoice == "Scissors") ||
+    (userchoice == "Paper" && ComputerChoice == "Rock") ||
+    (userchoice == "Scissors" && ComputerChoice == "Paper")
+  ) {
+    wins = "user Win 😎";
+    usercount++;
+
+    
+  } else if (
+    (userchoice == "Scissors" && ComputerChoice == "Rock") ||
+    (userchoice == "Rock" && ComputerChoice == "Paper") ||
+    (userchoice == "Paper" && ComputerChoice == "Scissors")
+  ) {
+    wins = "Computer Win 😒";
+    computercount++;
+   
+  } else {
+    draw = "Draw !😑";
+  }
+
+  let won = document.getElementById("won");
+  won.innerText = wins;
+  let userch = document.getElementById("userch");
+  userch.innerText = "User Choice is : " + userchoice;
+
+  let Computerch = document.getElementById("Computerch");
+  Computerch.innerText = "Computer Choice is : " + ComputerChoice;
+
+  let uWin = document.getElementById("uWin");
+
+  uWin.innerText = "User Wins : " + usercount;
+
+  let cWin = "Computer Wins : " + computercount;
+
+  let Draw = document.getElementById("Draw");
+
+  Draw.innerText = draw;
+}
